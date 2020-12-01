d0e7da0caebc187cbd8b94133f081d2527ab0074
MariaVeronicaR <mvrm10@gmail.com>
2020-11-30 21:32:01 -0600

    fix6

--- src/test/java/com/mayab/calidad/funcional/TestCrud.java
+++ src/test/java/com/mayab/calidad/funcional/TestCrud.java
@@ -24,12 +24,15 @@ public class TestCrud {
 
   @Before
   public void setUp() throws Exception {
-
-	  		baseUrl = "https://mern-crud.herokuapp.com";
-			System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-			driver = new ChromeDriver();
-			driver.manage().deleteAllCookies();
-			driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	  System.setProperty("webdriver.chrome.driver", "C:\\Users\\Usuario\\Desktop\\6to_semestre\\Calidad y pruebas de software\\chromedriver_win32\\chromedriver.exe");
+	    driver = new ChromeDriver();
+	    baseUrl = "https://www.google.com/";
+	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+//	  		baseUrl = "https://mern-crud.herokuapp.com";
+//			System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+//			driver = new ChromeDriver();
+//			driver.manage().deleteAllCookies();
+//			driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
   }
 
   
@@ -85,24 +88,7 @@ public class TestCrud {
 	    driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[2]/div/input")).clear();
 	    driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[2]/div/input")).clear();
 	    driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[2]/div/input")).sendKeys("prueba2@gmail.com");
-	    driver.findElement(By.xpath("//div[3]/div[2]/div/div")).click();
-	    driver.findElement(By.xpath("//div[3]/span")).click();
-	    driver.findElement(By.xpath("//form/button")).click();
-	    driver.findElement(By.xpath("//div[4]")).click();
-	    driver.findElement(By.xpath("//div[4]/div/p")).click();
-	    driver.findElement(By.xpath("//div[4]/div/p")).click();
-	    driver.findElement(By.xpath("//div[4]/div/p")).click();
-	    driver.findElement(By.xpath("//div[4]/div/p")).click();
-	    driver.findElement(By.xpath("//div[4]/div/p")).click();
-	    driver.findElement(By.xpath("//div[4]/div/p")).click();
-	    driver.findElement(By.xpath("//div[4]/div/p")).click();
-	    driver.findElement(By.xpath("//div[4]/div/p")).click();
-	    driver.findElement(By.xpath("//div[4]/div/p")).click();
-	    driver.findElement(By.xpath("//div[4]/div/p")).click();
-	    driver.findElement(By.xpath("//div[4]/div/p")).click();
-	    driver.findElement(By.xpath("//div[4]/div/p")).click();
-	    driver.findElement(By.xpath("//div[4]/div/p")).click();
-	    driver.findElement(By.xpath("//form/button")).click();
+	    driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/button")).click();
 	    
 	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
