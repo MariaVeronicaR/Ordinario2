788d67b3ad44e6cf36082ecc44c859ed59ffd8db
MariaVeronicaR <mvrm10@gmail.com>
2020-11-30 21:51:24 -0600

    fix8

--- src/test/java/com/mayab/calidad/funcional/TestCrud.java
+++ src/test/java/com/mayab/calidad/funcional/TestCrud.java
@@ -21,7 +21,15 @@ public class TestCrud {
   private boolean acceptNextAlert = true;
   private StringBuffer verificationErrors = new StringBuffer();
 
+  public void sleep(double d) {
 
+      try {
+          // Tiempo que va a dormir las funciones en milisegundos
+          Thread.sleep((long) (d * 1000));
+      } catch (InterruptedException ex) {}
+
+  }
+  
   @Before
   public void setUp() throws Exception {
 //	  System.setProperty("webdriver.chrome.driver", "C:\\Users\\Usuario\\Desktop\\6to_semestre\\Calidad y pruebas de software\\chromedriver_win32\\chromedriver.exe");
@@ -45,17 +53,16 @@ public class TestCrud {
 	    driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 	    driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[1]/div/input")).click();
 	    driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[1]/div/input")).clear();
-	    driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[1]/div/input")).sendKeys("Veeeronica");
+	    driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[1]/div/input")).sendKeys("Veeervonica");
 	    driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[2]/div/input")).click();
 	    driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[2]/div/input")).clear();
-	    driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[2]/div/input")).sendKeys("Vereeonica@gmail.com");
+	    driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[2]/div/input")).sendKeys("Vervevvveonica@gmail.com");
 	    driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[3]/div[1]/div/input")).click();
 	    driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[3]/div[1]/div/input")).clear();
 	    driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[3]/div[1]/div/input")).sendKeys("18");
-	   
+	    sleep(1.2);
 	    driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/button")).click();
-	   
-	    
+	    sleep(1.2);
 	    assertEquals("Successfully added!", driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[4]/div/p")).getText());
 		
   }
@@ -66,15 +73,16 @@ public class TestCrud {
 	  driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody/tr/td[5]/button")).click();
 	    driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[1]/div/input")).click();
 	    driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[1]/div/input")).clear();
-	    driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[1]/div/input")).sendKeys("prueba");
+	    driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[1]/div/input")).sendKeys("prueffba");
 	    driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[2]/div/input")).click();
 	    driver.findElement(By.xpath("//div[2]/div/div[2]")).click();
 	    driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[2]/div/input")).clear();
 	    driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[2]/div/input")).clear();
-	    driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[2]/div/input")).sendKeys("prueba2@gmail.com");
-	    driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/button")).click();
-	    
-	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	    driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[2]/div/input")).sendKeys("prueffba2@gmail.com");
+	    sleep(1.2);
+	    driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/button")).click(); 
+	    sleep(1.2);
+
 
 	    
 	    assertEquals("Successfully updated!", driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[4]/div/p")).getText());
